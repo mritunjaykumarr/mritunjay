a4d707b9358a98f8776c61b9d56c78a06f7081b7
Mritunjay kumar <149924432+mritunjaykumarr@users.noreply.github.com>
2026-03-09 01:09:19 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,1 @@
-// ============================================================
-//  firebase-config.js  — Shared by index.html & dashboard.html
-// ============================================================
-
-const FIREBASE_CONFIG = {
-  apiKey:            "AIzaSyBECM26idkr26XWDj6IY0e5u0TikYHYQKg",
-  authDomain:        "mritunjay-portfolio-25a6b.firebaseapp.com",
-  projectId:         "mritunjay-portfolio-25a6b",
-  storageBucket:     "mritunjay-portfolio-25a6b.firebasestorage.app",
-  messagingSenderId: "643363777685",
-  appId:             "1:643363777685:web:28ca07ddf4017820f13f9c",
-  measurementId:     "G-RPCJQZ2RQ0"
-};
-
-// Admin password for dashboard access (change this!)
-const ADMIN_PASSWORD = "mritunjay2025";
+//
