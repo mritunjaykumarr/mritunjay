50e2d842dc1ec3f111608d6451fd2da353c93ba8
Mritunjay kumar <149924432+mritunjaykumarr@users.noreply.github.com>
2026-03-09 12:17:35 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
-const FIREBASE_CONFIG = {
-  apiKey:            "AIzaSyBECM26idkr26XWDj6IY0e5u0TikYHYQKg",
-  authDomain:        "mritunjay-portfolio-25a6b.firebaseapp.com",
-  projectId:         "mritunjay-portfolio-25a6b",
-  storageBucket:     "mritunjay-portfolio-25a6b.firebasestorage.app",
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBECM26idkr26XWDj6IY0e5u0TikYHYQKg",
+  authDomain: "mritunjay-portfolio-25a6b.firebaseapp.com",
+  projectId: "mritunjay-portfolio-25a6b",
+  storageBucket: "mritunjay-portfolio-25a6b.firebasestorage.app",
   messagingSenderId: "643363777685",
-  appId:             "1:643363777685:web:28ca07ddf4017820f13f9c",
-  measurementId:     "G-RPCJQZ2RQ0"
+  appId: "1:643363777685:web:28ca07ddf4017820f13f9c",
+  measurementId: "G-RPCJQZ2RQ0"
 };
 
-const ADMIN_PASSWORD = "mritunjay2025";
+window.ADMIN_PASSWORD = "mritunjay2025";
